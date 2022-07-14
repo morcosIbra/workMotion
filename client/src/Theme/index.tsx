1b7fa879642a868562e033da1fbfa24d4ec08147
morcos <mdanielibra@gmail.com>
2022-07-14 13:25:30 +0200

    create theme colors

---
+++ client/src/Theme/index.tsx
@@ -0,0 +1,11 @@
+const theme = {
+  colors:{
+    primary:'#4169e1',
+    lightPrimary: '#bec5d8',
+    grey: '#a19a9a',
+    lightGrey: '#f2f2f2',
+    error: '#f44336'
+  }
+}
+
+export default theme;
