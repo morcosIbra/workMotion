5e1d638c818d31ff14882ee9ce85583815653ff6
morcos <mdanielibra@gmail.com>
2022-07-14 13:50:31 +0200

    define data type in employees request

--- client/src/axios/employeesRequests.tsx
+++ client/src/axios/employeesRequests.tsx
@@ -6,5 +6,5 @@ const employeesAxios = axios.create({
 
 export const fetchEmployeesRequest = () => employeesAxios.get<EmployeeProfile[]>("");
 
-export const updateEmployeeRequest = (id:number, data:{}) =>
+export const updateEmployeeRequest = (id: number, data: Record<string, unknown>) =>
   employeesAxios.patch<EmployeeProfile>(`/${id}`, data);
