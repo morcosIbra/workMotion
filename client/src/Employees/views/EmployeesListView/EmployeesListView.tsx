31c13a6b7148e8b63cf42d8eada75caa614fac14
morcos <mdanielibra@gmail.com>
2022-07-14 13:25:08 +0200

    create employees view

---
+++ client/src/Employees/views/EmployeesListView/EmployeesListView.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "../../../common/components/Table";
+import EmployeesListContainer from "../../containers/EmployeesListContainer";
+
+const EmployeesListView = () => {
+  return (
+    <>
+      <EmployeesListContainer></EmployeesListContainer>
+    </>
+  );
+};
+
+export default EmployeesListView;
