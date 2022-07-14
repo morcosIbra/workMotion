33a2551c5888674026a1cb0a4f2a57864d1fd552
morcos <mdanielibra@gmail.com>
2022-07-14 13:55:19 +0200

    change index to tsx format && add ErrorBoundary, styled components wrapper, redux wrapper

---
+++ client/src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import { store } from './store';
+import theme from './Theme';
+import ErrorBoundary from './common/Container/ErrorBoundaries';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </ErrorBoundary>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
